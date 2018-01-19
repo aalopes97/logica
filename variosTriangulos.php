c6a9f933fcb905bee0b6abaca335934486da31b1
php 20 <php.20@penadinho.cec.ime.usp.br>
2018-01-19 21:01:30 -0200

    programa exibe varios triangulos

--- variosTriangulos.php
+++ variosTriangulos.php
@@ -1,5 +1,8 @@
 <?php
+$varios = 10;
 
+
+while($cont < $varios){
 for($i=1;  $i<=5; $i++)  {
      $k = $i;
       for($j=0; $j<$k; $j++) {
@@ -7,5 +10,8 @@ for($i=1;  $i<=5; $i++)  {
         }
         echo "\n";
     }
+    echo "\n";
+$cont++;
+  }
 
  ?>
